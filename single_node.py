65b227f5538f352914025f31797bb839993a650a
John Lyu <paleneutron@outlook.com>
2020-10-29 14:54:59 +0800

    single node

--- single_node.py
+++ single_node.py
@@ -22,7 +22,7 @@ from io import StringIO
 
 Net = FLModel
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -35,8 +35,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, (batch_idx + 1) * len(data), len(train_loader.dataset),
                 100. * (batch_idx + 1) / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
+
 
 
 def test(model, device, test_loader):
@@ -90,17 +89,8 @@ def main():
 
     torch.manual_seed(args.seed)
 
-    device = torch.device("cuda" if use_cuda else "cpu")
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
-    if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
-        print("use cuda with {}".format(cuda_kwargs))
+
 
 #     transform=transforms.Compose([
 #         transforms.ToTensor(),
@@ -113,30 +103,53 @@ def main():
 #     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
 #     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
+
+
+
+
+        
+def run(epochs=500, lr=0.01, batch_size=1024, test_batch_size=1024, use_cuda=True, gamma=0.99, save_model=False):
+    train_kwargs = {'batch_size': batch_size}
+    test_kwargs = {'batch_size': test_batch_size}
+    if use_cuda and torch.cuda.is_available():
+        cuda_kwargs = {'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+        print("use cuda with {}".format(cuda_kwargs))
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    
     ud = UserRoundData()
     X, y = ud.all_data()
     X_train, X_test, y_train, y_test = train_test_split(X,
                                                         y,
                                                         test_size=0.3,
                                                         random_state=66)
+
+    
     train_loader = torch.utils.data.DataLoader(
         TensorDataset(torch.tensor(X_train), torch.tensor(y_train)),
         **train_kwargs)
     test_loader = torch.utils.data.DataLoader(
         TensorDataset(torch.tensor(X_test), torch.tensor(y_test)),
         **test_kwargs)
+    
+    
     model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    optimizer = optim.Adadelta(model.parameters(), lr=lr)
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+    scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, optimizer, epoch)
         test(model, device, test_loader)
         scheduler.step()
 
-    if args.save_model:
+    if save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
-
-
+        
+        
 if __name__ == '__main__':
     main()
